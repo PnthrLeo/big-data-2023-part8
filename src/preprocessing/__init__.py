371801f12cb1b2c6aa2ce6f355a5b7fd991bca10
PnthrLeo <leon-1402@mail.ru>
2023-06-28 14:30:03 +0300

    remove preprocessing from python scripts

--- src/preprocessing/__init__.py
+++
@@ -1,5 +0,0 @@
-from preprocessing.load_data import load_csv_data
-from preprocessing.transform import transform_data
-
-
-__all__ = ['load_csv_data', 'transform_data']
--- src/preprocessing/load_data.py
+++
@@ -1,5 +0,0 @@
-from pyspark.sql import SparkSession, DataFrame
-
-
-def load_csv_data(spark: SparkSession, path: str) -> DataFrame:
-    return spark.read.csv(path, header=True, sep='\t')
